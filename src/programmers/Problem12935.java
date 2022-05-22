6d124db0b0637e149f3726d935bf5bb9fe85c5bb
nokh9503 <nokh9503@hanmail.net>
2022-05-23 00:08:44 +0900

    add programmers problem 12935 제일 작은 수 제거하기(재확인)

---
+++ src/programmers/Problem12935.java
@@ -0,0 +1,50 @@
+package programmers;
+
+
+import java.util.Arrays;
+
+/**
+ * 프로그래머스 제일 작은 수 제거하기
+ * 유형: 배열
+ * 난이도: LV1
+ */
+
+public class Problem12935 {
+
+    public static void main(String[] args) {
+
+        Solution12935 solution12935 = new Solution12935();
+
+        int arr[] = {4, 3, 2, 1};
+    }
+}
+
+class Solution12935 {
+    public int[] solution(int[] arr) {
+
+       if (arr.length == 1) {
+           return new int[]{-1};
+       }
+
+       int minIdx = 0;
+
+       for (int i = 0; i < arr.length; i++) {
+           if (arr[minIdx] > arr[i]) {
+               minIdx = i;
+           }
+       }
+
+       int[] answer = new int[arr.length-1];
+       int cnt = 0;
+
+       for (int i = 0; i < arr.length; i++) {
+           if (i == minIdx) {
+               continue;
+           }
+           answer[cnt++] = arr[i];
+       }
+       Ara
+
+       return answer;
+    }
+}
