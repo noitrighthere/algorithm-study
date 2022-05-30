517a86fbb80758c36458f9ca7dd6a2bed886ca00
nokh9503 <nokh9503@hanmail.net>
2022-05-31 00:46:36 +0900

    solve programmers problem 12932 자연수 뒤집어 배열로 만들기

--- src/programmers/Problem12932.java
+++ src/programmers/Problem12932.java
@@ -7,4 +7,27 @@ package programmers;
  */
 
 public class Problem12932 {
+
+    public static void main(String[] args) {
+        Solution12932 solution12932 = new Solution12932();
+        solution12932.solution(12345);
+    }
+}
+
+class Solution12932 {
+    public int[] solution(long n) {
+
+        String str = String.valueOf(n);
+
+        StringBuilder sb = new StringBuilder(str);
+        String[] reverse = sb.reverse().toString().split("");
+
+        int[] answer = new int[reverse.length];
+
+        for (int i = 0; i < reverse.length; i++) {
+            answer[i] = Integer.parseInt(reverse[i]);
+        }
+
+        return answer;
+    }
 }
