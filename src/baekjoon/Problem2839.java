6da900265f374212e088c46240694431e6f918f2
nokh9503 <nokh9503@hanmail.net>
2022-06-14 00:20:06 +0900

    add baekjoon problem 2839 설탕 배달(solved)

--- src/baekjoon/Problem2839.java
+++ src/baekjoon/Problem2839.java
@@ -1,5 +1,9 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 백준 2839번 설탕 배달
  * 유형: 그리디
@@ -7,7 +11,29 @@ package baekjoon;
  */
 
 public class Problem2839 {
-    public static void main(String[] args) {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());    // 설탕 무게
+        int result = 0;
+
+        while (true) {
+
+            if (N % 5 == 0) {
+                result = result + (N/5);
+                System.out.println(result);
+                break;
+            }
+
+            N = N - 3;
+            result += 1;
+
+            if (N < 0) {
+                result = -1;
+                System.out.println(result);
+                break;
+            }
+        }
     }
 }
