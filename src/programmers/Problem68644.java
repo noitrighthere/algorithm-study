f86826656e3e2569a66c4bf67d380fbdbd449ce4
nokh9503 <nokh9503@hanmail.net>
2022-06-24 23:48:26 +0900

    add programmers problem 68644 두 개 뽑아서 더하기(not solved)

---
+++ src/programmers/Problem68644.java
@@ -0,0 +1,24 @@
+package programmers;
+
+/**
+ * 프로그래머스 두 개 뽑아서 더하기
+ * 월간 코드 챌린지 시즌1
+ * 난이도: LV1
+ */
+
+public class Problem68644 {
+    public static void main(String[] args) {
+        Solution68644 solution68644 = new Solution68644();
+        solution68644.solution(new int[]{2, 1, 3, 4, 1});
+    }
+}
+
+class Solution68644 {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+}
