603dd3025b6c27437b5aed4fae59911cb988a71d
nokh9503 <nokh9503@hanmail.net>
2022-06-25 00:00:20 +0900

    add programmers problem 68644 두 개 뽑아서 더하기(solved)

--- src/programmers/Problem68644.java
+++ src/programmers/Problem68644.java
@@ -1,5 +1,9 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * 프로그래머스 두 개 뽑아서 더하기
  * 월간 코드 챌린지 시즌1
@@ -15,9 +19,25 @@ public class Problem68644 {
 
 class Solution68644 {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
 
+        int temp = 0;
+        List<Integer> arr = new ArrayList<>();
 
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                temp = numbers[i] + numbers[j];
+                if (!arr.contains(temp)) {
+                    arr.add(temp);
+                }
+            }
+        }
+
+        Collections.sort(arr);
+        int[] answer = new int[arr.size()];
+
+        for (int i = 0; i < arr.size(); i++) {
+            answer[i] = arr.get(i);
+        }
 
         return answer;
     }
