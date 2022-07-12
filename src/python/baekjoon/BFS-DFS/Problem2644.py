c041faa5db7c92c9097f3970c1e1b19be2d44c59
nokh9503 <nokh9503@hanmail.net>
2022-07-13 00:19:37 +0900

    add baekjoon problem 2664 촌수계산(python, solved)

--- src/python/baekjoon/BFS-DFS/Problem2644.py
+++ src/python/baekjoon/BFS-DFS/Problem2644.py
@@ -1,32 +1,42 @@
+from collections import deque
+
 # 백준 2644번 촌수계산
 # 유형: DFS/BFS
 # 난이도: 실버2
 
-def dfs(a, b):
-    global count
+# a: start, b: end
+def bfs(a, b):
+    count = 0
+    q = deque()
+    q.append((a, count))
 
-    visited[a] = True
+    while q:
+        a, count = q.popleft()
 
-    for e in graph[a]:
-        if not visited[e]:
+        # 목적지에 도착했을 때 count를 리턴
+        if a == b:
+            return count
+
+        if not visited[a]:
             count += 1
-            if a == b:
-                break;
-            dfs(e, count)
+            visited[a] = True
+            for e in graph[a]:
+                if not visited[e]:
+                    q.append((e, count))
+
+    return -1
 
 n = int(input())    # 전체 사람의 수 입력
-x, y = map(int, input().split())    # 촌수를 계산해야 하는 두 사람의 번호
+a, b = map(int, input().split())    # 촌수를 계산해야 하는 두 사람의 번호
 m = int(input())    # 관계의 수 입력
 
 graph = [[] for _ in range(n+1)]
 
+# 관계 입력
 for _ in range(m):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-print(graph)
 visited = [False] * (n+1)
-count = 0
-dfs(x, y)
-print(count)
+print(bfs(a, b))
