e51a3316e22c68efaa39aee05eea310fb6b94600
nokh9503 <nokh9503@hanmail.net>
2022-07-30 17:51:51 +0900

    add baekjoon problem 1931 회의실 배정(python)

---
+++ src/python/baekjoon/greedy/Problem1931.py
@@ -0,0 +1,25 @@
+# 백준 1931번 회의실 배정
+# 유형: 그리디
+# 난이도: 실버1
+
+N = int(input())
+arr = []
+
+for _ in range(N):
+    start, end = map(int, input().split())
+    arr.append([start, end])
+
+last = 0
+cnt = 0
+
+arr.sort(key=lambda x:x[0])
+arr.sort(key=lambda x:x[1])
+
+print(arr)
+
+for i, j in arr:
+    if i >= last:
+        cnt += 1
+        last = j
+
+print(cnt)
